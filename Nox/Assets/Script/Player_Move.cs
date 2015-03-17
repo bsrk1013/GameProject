f83f6ab716748ccec0622ba140b6e107943144eb
Baeck <bsrk1013@naver.com>
2015-03-18 01:42:15 +0900

    * 3.18 업데이트

--- Nox/Assets/Script/Player_Move.cs
+++ Nox/Assets/Script/Player_Move.cs
@@ -6,6 +6,7 @@ public class Player_Move : MonoBehaviour {
 	public int Player_Speed = 0;
 	public int Player_JumpPower = 0;
 	private Rigidbody2D rgBody = null;
+	private bool isGround = true;
 
 	// Use this for initialization
 	void Start ()
@@ -14,11 +15,34 @@ public class Player_Move : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
 	{
+		//float move =Input.GetAxisRaw("Horizontal");
+		//rgBody.velocity = new Vector2(move * Player_Speed, rgBody.velocity.y );
+		Moving ();
+		Jump ();
+	}
+
+	void Moving()
+	{
+		if (Input.GetKey (KeyCode.D))
+		{
+			transform.Translate( Vector2.right * Player_Speed * Time.deltaTime );
+			transform.eulerAngles = new Vector2( 0.0f, 0.0f );
+		}
+
 		if (Input.GetKey (KeyCode.A))
 		{
-			transform.position += new Vector3(  )
+			transform.Translate( Vector2.right * Player_Speed * Time.deltaTime );
+			transform.eulerAngles = new Vector2( 0.0f, 180.0f );
+		}
+	}
+
+	void Jump()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && isGround)
+		{
+			rgBody.AddForce( new Vector2( 0.0f, Player_JumpPower ) );
 		}
 	}
 }
