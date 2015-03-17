1bd92baf6956180474eb28e1925ac0b161ebd0da
Baeck <bsrk1013@naver.com>
2015-03-18 00:01:38 +0900

    * Player 객체 생성 및 움직임 구현

---
+++ Nox/Assets/Script/Player_Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Move : MonoBehaviour {
+
+	public int Player_Speed = 0;
+	public int Player_JumpPower = 0;
+	private Rigidbody2D rgBody = null;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rgBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.A))
+		{
+			transform.position += new Vector3(  )
+		}
+	}
+}
