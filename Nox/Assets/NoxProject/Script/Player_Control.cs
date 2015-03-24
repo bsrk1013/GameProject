8b7d951ebe2475c4a38bbb1b67fe5655b6812fda
Baeck <bsrk1013@naver.com>
2015-03-25 00:54:16 +0900

    2015.03.25 * 케릭터 애니메이션 구현 * 타일맵 구현( 완벽하지 않음 )

--- Nox/Assets/NoxProject/Script/Player_Control.cs
+++ Nox/Assets/NoxProject/Script/Player_Control.cs
@@ -3,25 +3,44 @@ using System.Collections;
 
 public class Player_Control : MonoBehaviour {
 
-	public int Player_Speed = 0;
-	public int Player_JumpPower = 0;
-	private Rigidbody2D rgBody = null;
-	private bool isJumped = false;
+	// 플레이어 상태
+	private int STATE_IDLE = 0;
+	private int STATE_JUMP = 1;
+	private int STATE_MOVE = 2;
+
+	public int Player_Speed = 0;		//플레이어 속도
+	public int Player_JumpPower = 0;	//플레이어 점프력
+	private bool isJumped = false;		//점프체크
+	private Rigidbody2D rgBody = null;	
+	private Animator ani = null;
+	private BoxCollider2D bc2D = null;
 
 	// Use this for initialization
 	void Start ()
 	{
+		//리지드바디, 애니메이션 할당
 		rgBody = GetComponent<Rigidbody2D> ();
+		ani = GetComponent<Animator> ();
+		bc2D = GetComponent<BoxCollider2D> ();
+		//케릭터의 회전에 의해 조명이 같이 회전하여 2개를 설치함
+		transform.FindChild ("match1").transform.position = new Vector3( transform.position.x, transform.position.y, 1.0f );
+		transform.FindChild ("match2").transform.position = new Vector3( transform.position.x, transform.position.y, -1.0f );
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		//float move =Input.GetAxisRaw("Horizontal");
-		//rgBody.velocity = new Vector2(move * Player_Speed, rgBody.velocity.y );
-		transform.FindChild ("match1").transform.position = new Vector3( transform.position.x, transform.position.y, 1.0f );
-		transform.FindChild ("match2").transform.position = new Vector3( transform.position.x, transform.position.y, -1.0f );
-		Moving ();
+		if (!isJumped)
+		{	//평상시
+			Change_State( STATE_IDLE );
+		}
+
+		//Debug.Log ( rgBody.velocity.y );
+		Moving ();	
+	}
+
+	void Update()
+	{
 		Jump ();
 	}
 
@@ -31,11 +50,15 @@ public class Player_Control : MonoBehaviour {
 		{
 			transform.Translate( Vector2.right * Player_Speed * Time.deltaTime );
 			transform.eulerAngles = new Vector2( 0.0f, 0.0f );
+			transform.position = new Vector3( transform.position.x, transform.position.y, 0.0f );
+			Change_State( STATE_MOVE );
 		}
 		if (Input.GetKey (KeyCode.A))
 		{
 			transform.Translate( Vector2.right * Player_Speed * Time.deltaTime );
 			transform.eulerAngles = new Vector2( 0.0f, 180.0f );
+			transform.position = new Vector3( transform.position.x, transform.position.y, 0.0f );
+			Change_State( STATE_MOVE );
 		}
 	}
 
@@ -43,18 +66,36 @@ public class Player_Control : MonoBehaviour {
 	{
 		if (rgBody.velocity.y != 0)
 		{
+			Change_State( STATE_JUMP );
 			isJumped = true;
 		}
 
-		if (isJumped && rgBody.velocity.y == 0)
+		if (isJumped && rgBody.velocity.y == 0 )
 		{
 			isJumped = false;
 		}
 
 		if (Input.GetKeyDown (KeyCode.Space) && !isJumped)
 		{
-			rgBody.AddForce( new Vector2( 0.0f, Player_JumpPower ) );
+			rgBody.AddForce( transform.up * Player_JumpPower );
 			isJumped = true;
 		}
+	}
+
+	bool Change_State( int _State )
+	{
+		if (STATE_IDLE == _State) {
+			ani.SetInteger ("State", STATE_IDLE);
+		} else if (STATE_JUMP == _State) {
+			ani.SetInteger( "State", STATE_JUMP );
+		} else if( STATE_MOVE == _State ){
+			if( isJumped )
+			{
+				return false;
+			}
+			ani.SetInteger( "State", STATE_MOVE );
+		}
+
+		return true;
 	}
 }
---
+++ Nox/Assets/NoxProject/Script/TileManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+	public GameObject Tile;
+	private Vector2 [ , ] TilePos = new Vector2[12,16];
+	private int[ , ] TileMap =
+	{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+	// Use this for initialization
+	void Start () {
+		// 오브젝트가 배치될 위치를 배열에 저장함
+		for( int a = 0; 12 > a; ++a )
+		{
+			for( int b = 0; 16 > b; ++b )
+			{
+				TilePos[ a, b ] = new Vector2( -5.12f + ( b * 0.64f ), 3.84f + ( -a * 0.64f ) );
+				Debug.Log(TilePos[ a, b ]);
+			}
+		}
+
+		for( int a = 0; 12 > a; ++a )
+		{
+			for( int b = 0; 16 > b; ++b )
+			{
+				if( TileMap[a, b] == 1 )
+				{
+					Instantiate( Tile, new Vector3( TilePos[ a, b ].x + 0.32f, TilePos[ a, b ].y - 0.32f, 0.0f ), Quaternion.identity );
+				}
+			}
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
